947546c0911340d9fd9c0b64e6510a104e294477
Mateus Sátiro <mateus_sat@hotmail.com>
2024-03-05 21:37:48 -0300

    feat: archive with not utility

--- posts/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from posts import models
-
-
-class PostsSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = models.Posts
-        fields = '__all__'
